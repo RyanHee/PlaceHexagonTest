0fe259d4f257287408dd044fdbcfebd5f1510c42
Ryan <heyuanboryan814@outlook.com>
2024-02-27 09:32:08 -0600

    Changed some stuff, simplified it more

--- src/Panel.java
+++ src/Panel.java
@@ -26,7 +26,8 @@ public class Panel extends JPanel implements ActionListener {
             System.out.println(1231);
         }
         angle=0;
-        test=new node("", "jaaj");
+        test=new node("");
+
         //hexButton.setBounds(200*getWidth()/1600, 100*getHeight()/900, 50, 50);
         //hexButton.setVisible(true);
 
@@ -37,36 +38,38 @@ public class Panel extends JPanel implements ActionListener {
         super.paint(g);
         xPoints=new int[6];
         yPoints=new int[6];
-        int x=300*getWidth()/1600;
-        int y = 300*getHeight()/1600;
-        int w = 100*getWidth()/1600;
-        int h = 116*getHeight()/900;
+        int x = 300;
+        int y = 300;
+        int w = 58;
+        int h = 58;
+        System.out.println("w: "+w+", h: "+h);
         for(int i = 0; i < 6; i++) {
             double v = i*Math.PI/3;
             //use this for ^
             xPoints[i] = x+w/2+(int)Math.round(-w/2*Math.cos(v + Math.PI/2));
-            yPoints[i] = y+h/2+(int)Math.round(-h/2*Math.sin(v + Math.PI/2));
+            yPoints[i] = y+w/2+(int)Math.round(-w/2*Math.sin(v + Math.PI/2));
             //use this for ------
             //xPoints[i] = x + (int)Math.round(-width*Math.sin(v + Math.PI/2));
             //yPoints[i] = y + (int)Math.round(-height*Math.cos(v + Math.PI/2));
         }
+        w=50;
         System.out.println(Arrays.toString(xPoints));
         System.out.println(Arrays.toString(yPoints));
-        g.drawImage(img, x, y, w, h,null);
+        g.fillPolygon(new Polygon(xPoints, yPoints, 6));
         //右下
-        g.drawImage(img1, xPoints[0], yPoints[2]+h/35, w,h,null);
+        g.drawImage(img1, xPoints[0], yPoints[2], w,h,null);
         //右上
-        g.drawImage(img1, xPoints[0], yPoints[1]-y/116*100, w,h,null);
+        g.drawImage(img1, xPoints[0], yPoints[1]-h, w,h,null);
         //右
-        g.drawImage(img1, xPoints[1]+w/15, yPoints[0], w,h,null);
+        g.drawImage(img1, xPoints[1], yPoints[0], w,h,null);
         //左下
-        g.drawImage(img1, xPoints[0]-w, yPoints[2]+h/35, w,h,null);
+        g.drawImage(img1, xPoints[0]-w, yPoints[2], w,h,null);
         //左上
-        g.drawImage(img1, xPoints[0]-w, yPoints[1]-y/116*100, w,h,null);
+        g.drawImage(img1, xPoints[0]-w, yPoints[1]-h, w,h,null);
         //左
-        g.drawImage(img1, xPoints[5]-w-w/15, yPoints[0], w,h,null);
+        g.drawImage(img1, xPoints[5]-w, yPoints[0], w,h,null);
 
-        test.setBounds(800*getWidth()/1600, 450*getHeight()/900, w, h);
+        test.setBounds(800*getWidth()/1600, 450*getHeight()/900, w, w);
         test.paintComponent(g);
 
 
--- src/node.java
+++ src/node.java
@@ -6,68 +6,39 @@ import java.util.Arrays;
 
 public class node extends JButton {
 
-    private String val;
-    private BufferedImage img, outline;
-    private boolean isPlaced;
     private Polygon hexagon;
-    private int[]xPoints, yPoints;
+    private String val;
     private int x, y, width, height;
-    private node[]neighbors;
-
+    private node[] neighbors;
+    private int[]xPoints, yPoints;
+    private boolean isPlaced;
+    private BufferedImage img, outline;
     public node(String label){
         super(label);
-        val = "";
-        try{
-            img = ImageIO.read(Panel.class.getResource("tile1.png"));
-            outline = ImageIO.read(Panel.class.getResource("tileOutline.png"));
-        }
-        catch (Exception e){
-            System.out.println("fuck");
-        }
-        xPoints=new int[6];
-        yPoints=new int[6];
+        xPoints = new int[6];
+        yPoints = new int[6];
+        System.out.println(Arrays.toString(xPoints));
+        System.out.println(Arrays.toString(yPoints));
+        setOpaque(false);
+        setContentAreaFilled(false);
+        setBorderPainted(false);
         hexagon = new Polygon(xPoints, yPoints, 6);
-        isPlaced=false;
-        neighbors=new node[6];
-    }
-
-    public node(String label, String v){
-        super(label);
-        val = v;
         try{
             img = ImageIO.read(Panel.class.getResource("tile.png"));
-            outline = ImageIO.read(Panel.class.getResource("tileOutline.png"));
         }
         catch (Exception e){
             System.out.println("fuck");
         }
-        xPoints=new int[6];
-        yPoints=new int[6];
-        hexagon = new Polygon(xPoints, yPoints, 6);
+    }
+
+    public node(String label, String s){
+        super(label);
+        xPoints = new int[6];
+        yPoints = new int[6];
+        val=s;
         isPlaced=true;
-        neighbors=new node[6];
-        int idx=0;
-        //0<->3
-        //1<->4
-        //2<->5
-
-        for (node i:neighbors){
-            node n=new node("");
-            neighbors[idx]=n;
-            if (idx>2){
-                neighbors[idx].setNeighbor(this, idx-3);
-            }
-            else{
-                neighbors[idx].setNeighbor(this, idx+3);
-            }
-
-            idx++;
-        }
     }
 
-    public boolean getPlaced(){
-        return isPlaced;
-    }
 
     public void setNeighbor(node n, int idx){
         if (neighbors[idx]==null){
@@ -97,7 +68,6 @@ public class node extends JButton {
             System.out.println("this place is placed!!!");
         }
     }
-
     public void setVal(String v){
         val=v;
         isPlaced=true;
@@ -112,29 +82,11 @@ public class node extends JButton {
             System.out.println("fuck");
         }
     }
-
-    @Override
-    public void setBounds(int xx, int yy, int w, int h){
-        x=xx;
-        y=yy;
-        width=w;
-        height = h;
-        for(int i = 0; i < 6; i++) {
-            double v = i*Math.PI/3;
-            xPoints[i] = x +width/2+ (int)Math.round(-width/2*Math.cos(v + Math.PI/2));
-            yPoints[i] = y +height/2+ (int)Math.round(-height/2*Math.sin(v + Math.PI/2));
-        }
-        hexagon = new Polygon(xPoints, yPoints, 6);
-        //Graphics g = getGraphics();
-        //System.out.println(g);
-        //g.fillPolygon(hexagon);
-        System.out.println(Arrays.toString(xPoints));
-        System.out.println(Arrays.toString(yPoints));
-    }
-
     public void drawImg(Graphics g,int xx, int yy, int w, int h){
+        System.out.println();
+        System.out.println("w: "+w+", h: "+h);
         g.drawImage(img, xx, yy, w, h,null);
-        g.drawImage(outline, xx, yy, w, h, null);
+        //g.drawImage(outline, xx, yy, w, h, null);
         int[]nx=new int[6];
         int[]ny=new int[6];
         nx[0]=xPoints[0];
@@ -151,13 +103,13 @@ public class node extends JButton {
         ny[5]=yPoints[1]-h;
         for (int i=0;i<6;i++){
             node n = neighbors[i];
-            System.out.println(i);
+            //System.out.println(i);
             if (n==null){
-                System.out.println("bitch");
+                //System.out.println("bitch");
             }
             else{
-                System.out.println("wtf");
-                System.out.println(n);
+                //System.out.println("wtf");
+                //System.out.println(n);
                 if (!n.getPlaced()){
                     n.drawImg(g, nx[i],ny[i],w,h);
                 }
@@ -165,35 +117,56 @@ public class node extends JButton {
             }
         }
     }
-
-    public String getVal(){
-        return val;
+    public boolean getPlaced(){
+        return isPlaced;
+    }
+    protected void paintBorder(Graphics g) {
+        g.setColor(Color.BLACK);
+        int x0 = getSize().width/2;
+        int y0 = getSize().height/2;
+        for(int i=0; i<6; i++) {
+            double v = i*Math.PI/3;
+            xPoints[i] = x0 + (int)Math.round((getWidth()/2)*Math.sin(v));
+            yPoints[i] = y0 + (int)Math.round((getHeight()/2)*Math.cos(v));
+        }
+        g.drawPolygon(xPoints, yPoints,6);
     }
 
     protected void paintComponent(Graphics g) {
+        if (getModel().isArmed()) {
+            g.setColor(Color.lightGray);
+        } else {
+            g.setColor(getBackground());
+        }
+        int x0 = getSize().width/2;
+        int y0 = getSize().height/2;
+        for(int i=0; i<6; i++) {
+            double v = i*Math.PI/3;
+            xPoints[i] = x0 + (int)Math.round((getWidth()/2)*Math.sin(v));
+            yPoints[i] = y0 + (int)Math.round((getHeight()/2)*Math.cos(v));
+        }
+        System.out.println(Arrays.toString(xPoints));
+        System.out.println(Arrays.toString(yPoints));
+        g.fillPolygon(xPoints, yPoints, 6);
         super.paintComponent(g);
-        drawImg(g, x, y, width, height/100*116);
     }
+
+
 
     @Override
     public boolean contains(int x1, int y1) {
-        if (hexagon==null){
-            int[] xlst = new int[6];
-            int[] ylst = new int[6];
-
-            int x = getSize().width/2;
-            int y = getSize().height/2;
-            for (int i=0;i<6;i++){
-                double v = Math.PI/3;
-                xlst[i] = x + (int)Math.round((getWidth()/2)*Math.cos(v));
-                ylst[i] = y + (int)Math.round((getHeight()/2)*Math.sin(v));
+        if (hexagon == null ||
+                !hexagon.getBounds().equals(getBounds())) {
+            int x0 = getSize().width/2;
+            int y0 = getSize().height/2;
+            for(int i=0; i<6; i++) {
+                double v = i*Math.PI/3;
+                xPoints[i] = x0 + (int)Math.round((getWidth()/2)*Math.cos(v));
+                yPoints[i] = y0 + (int)Math.round((getHeight()/2)*Math.sin(v));
             }
-            hexagon=new Polygon(xlst, ylst, 6);
+            hexagon = new Polygon(xPoints,yPoints,6);
         }
         return hexagon.contains(x1, y1);
     }
 
-    public String toString(){
-        return isPlaced+" "+val;
-    }
 }
